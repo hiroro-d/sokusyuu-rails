e53433b1b73ff110efe4d519f442d5121b601d40
hiroro-d <hi22en.110@gmail.com>
2023-01-14 07:07:38 +0000

    permit2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
-  def login_required
-    redirect_to login_url unless current_user
-  end
+  # def login_required
+  #   redirect_to login_url unless current_user
+  # end
 end
